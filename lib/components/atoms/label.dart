e017b38624318bb46b24b98222e928f7c701001d
leticia-silva-rarolabs <leticia.silva@rarolabs.com.br>
2025-06-26 13:55:47 -0300

    qlabel

---
+++ lib/components/atoms/label.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class QLabel extends StatelessWidget {
+  final String text;
+  final TextStyle? style;
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final String? semanticsLabel;
+  final String? semanticsIdentifier;
+
+  const QLabel({
+    super.key,
+    required this.text,
+    this.style,
+    this.textAlign,
+    this.maxLines,
+    this.overflow = TextOverflow.ellipsis,
+    this.semanticsLabel,
+    this.semanticsIdentifier,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final typography = theme.textTheme;
+    final baseStyle = typography.bodyLarge?.copyWith(
+      color: theme.colorScheme.onSurface,  
+    );
+
+    return Text(
+      text,
+      style: baseStyle?.merge(style), 
+      textAlign: textAlign ?? TextAlign.start,
+      maxLines: maxLines,
+      overflow: overflow ?? TextOverflow.ellipsis,
+      semanticsLabel: semanticsLabel,
+      semanticsIdentifier: semanticsIdentifier,
+    );
+  }
+}
