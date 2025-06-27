295ac7079fef0a483d0f5971bc727a5d36a0be00
Guilherme Campos <guilherme.campos@rarolabs.com.br>
2025-06-27 10:07:27 -0300

    radiobutton

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
 [22] - Componente [RSnackBar]
 [23] - Componente [RBadge]
 [26] - Componente [RCheckbox]
+[33] - Componente [RRadioButton]
 
 ## Corrigido
 
--- lib/components/atoms/atoms.dart
+++ lib/components/atoms/atoms.dart
@@ -7,3 +7,4 @@ export 'circular_progress_indicator.dart';
 export 'linear_progress_indicator.dart';
 export 'switch.dart';
 export 'badge/badge.dart';
+export 'radio_button/radio_button.dart';
