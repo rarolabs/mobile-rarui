fb5f5b9860d990c9415a217d05fcbfce7f82d2ae
Caio <caio.guimaraes@rarolabs.com.br>
2025-07-03 08:32:31 -0300

    add import expansion tile

--- lib/components/molecules/molecules.dart
+++ lib/components/molecules/molecules.dart
@@ -4,6 +4,7 @@ export 'buttons/elevated_button.dart';
 export 'buttons/filled_button/filled_button.dart';
 export 'buttons/filled_button/filled_button_tonal.dart';
 export 'buttons/floating_action_button/floating_action_button.dart';
+export 'expansion_tile/expansion_tile.dart';
 export 'buttons/floating_action_button/floating_action_button_extended.dart';
 export 'buttons/icon_button.dart';
 export 'buttons/outlined_button.dart';
