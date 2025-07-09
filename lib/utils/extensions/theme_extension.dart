96fdf985f41ce32464a7fa2501c5fa9d0f71ff1c
Alessandro Baségio <mestredns@gmail.com>
2025-07-09 10:20:56 -0300

    Melhoria nos temas

--- lib/utils/extensions/theme_extension.dart
+++ lib/utils/extensions/theme_extension.dart
@@ -4,20 +4,24 @@ import 'package:flutter/material.dart';
 class ExtraColors extends ThemeExtension<ExtraColors> {
   final Color success;
   final Color warning;
+  final Color error;
 
   const ExtraColors({
     required this.success,
     required this.warning,
+    required this.error,
   });
 
   @override
   ExtraColors copyWith({
     Color? success,
     Color? warning,
+    Color? error,
   }) {
     return ExtraColors(
       success: success ?? this.success,
       warning: warning ?? this.warning,
+      error: error ?? this.error,
     );
   }
 
@@ -27,6 +31,7 @@ class ExtraColors extends ThemeExtension<ExtraColors> {
     return ExtraColors(
       success: Color.lerp(success, other.success, t)!,
       warning: Color.lerp(warning, other.warning, t)!,
+      error: Color.lerp(error, other.error, t)!,
     );
   }
 }
