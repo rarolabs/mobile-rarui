31326cf31a60021f18aa339e6a2cdb16859904ff
Alessandro Baségio <mestredns@gmail.com>
2025-07-17 11:23:02 -0300

    Melhoria no funcionamento do themeExtension, suporte a esquema de cor completo; Example Page - melhoria na organização dos componentes, suporte a novo esquema de cores;

--- lib/utils/extensions/theme_extension.dart
+++ lib/utils/extensions/theme_extension.dart
@@ -2,26 +2,30 @@ import 'package:flutter/material.dart';
 
 @immutable
 class ExtraColors extends ThemeExtension<ExtraColors> {
-  final Color success;
-  final Color warning;
-  final Color error;
+  final ColorScheme success;
+  final ColorScheme warning;
+  final ColorScheme error;
+  final ColorScheme info;
 
   const ExtraColors({
     required this.success,
     required this.warning,
     required this.error,
+    required this.info,
   });
 
   @override
   ExtraColors copyWith({
-    Color? success,
-    Color? warning,
-    Color? error,
+    ColorScheme? success,
+    ColorScheme? warning,
+    ColorScheme? error,
+    ColorScheme? info,
   }) {
     return ExtraColors(
       success: success ?? this.success,
       warning: warning ?? this.warning,
       error: error ?? this.error,
+      info: info ?? this.info,
     );
   }
 
@@ -29,9 +33,10 @@ class ExtraColors extends ThemeExtension<ExtraColors> {
   ExtraColors lerp(ThemeExtension<ExtraColors>? other, double t) {
     if (other is! ExtraColors) return this;
     return ExtraColors(
-      success: Color.lerp(success, other.success, t)!,
-      warning: Color.lerp(warning, other.warning, t)!,
-      error: Color.lerp(error, other.error, t)!,
+      success: ColorScheme.lerp(success, other.success, t),
+      warning: ColorScheme.lerp(warning, other.warning, t),
+      error: ColorScheme.lerp(error, other.error, t),
+      info: ColorScheme.lerp(info, other.info, t),
     );
   }
 }
