61d31a6fadd369f19513fa7fd9ea6ac7e84362cc
Alessandro Baségio <mestredns@gmail.com>
2025-07-17 16:28:39 -0300

    Toast adaptado a temas

--- lib/components/molecules/toast/toast.dart
+++ lib/components/molecules/toast/toast.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:rarUI/utils/extensions/theme_extension.dart';
 
 enum RToastType { clean, soft, solid }
 
 enum RToastStyle { success, warning, error, info }
 
-// TODO: Migrar cores e estilos para o tema
 class RToast extends StatelessWidget {
   final String title;
   final String message;
@@ -34,60 +34,46 @@ class RToast extends StatelessWidget {
 
     Icon icon;
     IconData iconData;
-    Color color;
+    ColorScheme colorScheme;
     double iconSize = isDense ? 16 : 24;
-
-    double backgroundOpacity;
-
-    switch (type) {
-      case RToastType.clean:
-        backgroundOpacity = 0.0;
-        break;
-      case RToastType.soft:
-        backgroundOpacity = 0.1;
-        break;
-      case RToastType.solid:
-        backgroundOpacity = 1.0;
-        break;
-    }
 
     switch (style) {
       case RToastStyle.success:
         iconData = Icons.check_circle;
-        color = Color(0xFF1A9338);
+        colorScheme = Theme.of(context).extension<ExtraColors>()!.success;
         break;
       case RToastStyle.warning:
         iconData = Icons.warning;
-        color = Color(0xFFFFB042);
+        colorScheme = Theme.of(context).extension<ExtraColors>()!.warning;
         break;
       case RToastStyle.error:
         iconData = Icons.error;
-        color = Color(0xFFFDC2A23);
+        colorScheme = Theme.of(context).extension<ExtraColors>()!.error;
         break;
       case RToastStyle.info:
         iconData = Icons.info;
-        color = Color(0xFF03AEFC);
+        colorScheme = Theme.of(context).extension<ExtraColors>()!.info;
         break;
     }
 
     icon = Icon(
       iconData,
       size: iconSize,
-      color: type == RToastType.solid ? onSurface : color,
+      color: type == RToastType.solid ? onSurface : colorScheme.primary,
     );
 
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          // Faixa lateral sem intrinsic height
-          // Avaliar se vale o contornar o intrinsic height assim
-          colors: [
-            color.withValues(alpha: type == RToastType.clean ? 1 : backgroundOpacity),
-            color.withValues(alpha: backgroundOpacity),
-          ],
-          stops: [0.01, 0.01],
-        ),
+        color: colorScheme.primary.withValues(alpha: type == RToastType.solid ? 1.0 : 0.1),
+        gradient: type == RToastType.clean
+            ? LinearGradient(
+                // Faixa lateral sem intrinsic height
+                // Avaliar se vale o contornar o intrinsic height assim
+                colors: [colorScheme.primary, Theme.of(context).colorScheme.surface],
+                stops: [0.01, 0.01],
+              )
+            : null,
       ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
