4a0e375b93244fef3cdf5fc816c93515856945ba
Nilson Soares <nilson.batista@rarolabs.com.br>
2025-07-25 10:42:51 -0300

    feat: cria o componente RSearchModal

---
+++ lib/components/organisms/search_modal/search_modal.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:rarUI/components/molecules/title_subtitle/title_subtitle.dart';
+import 'package:rarUI/rarui.dart';
+
+class RSearchModal<T extends Object> extends StatefulWidget {
+  const RSearchModal({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    this.searchHintText,
+    this.searchLabel,
+    required this.searchItems,
+    required this.onSelected,
+  });
+
+  final String title;
+  final String subtitle;
+  final String? searchHintText;
+  final String? searchLabel;
+  final List<RSearchModalItem<T>> searchItems;
+  final ValueChanged<T> onSelected;
+
+  @override
+  State<RSearchModal> createState() => _RSearchModalState<T>();
+}
+
+class _RSearchModalState<T extends Object> extends State<RSearchModal<T>>
+    with TickerProviderStateMixin {
+  late final AnimationController bottomSheetController;
+  late T? selectedValue;
+  late List<RSearchModalItem<T>> items;
+  @override
+  void initState() {
+    super.initState();
+    bottomSheetController = BottomSheet.createAnimationController(this);
+    items = widget.searchItems;
+  }
+
+  _filterItems(String value) {
+    setState(() {
+      items = widget.searchItems
+          .where(
+            (item) => item.label.toLowerCase().contains(value.toLowerCase()),
+          )
+          .toList();
+    });
+  }
+
+  @override
+  void dispose() {
+    bottomSheetController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomSheet(
+      animationController: bottomSheetController,
+      showDragHandle: true,
+      enableDrag: true,
+      builder: (context) => Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          spacing: 24.0,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RTitleSubtitle(
+              title: widget.title,
+              subTitle: widget.subtitle,
+              titleStyle: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+              ),
+              subTitleStyle: TextStyle(
+                fontSize: 14.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            RTextFormField(
+              autofocus: false,
+              decoration: InputDecoration(
+                hintText: widget.searchHintText,
+                hintStyle: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w400,
+                ),
+                labelText: widget.searchLabel,
+                labelStyle: TextStyle(fontWeight: FontWeight.w700),
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                _filterItems(value);
+              },
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemCount: items.length,
+                itemBuilder: (context, index) => RListTile(
+                  title: RLabel(
+                    text: items[index].label,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  onTap: () {
+                    widget.onSelected(items[index].value);
+                    Navigator.of(context).pop();
+                  },
+                ),
+                separatorBuilder: (context, index) => RDivider(),
+              ),
+            ),
+          ],
+        ),
+      ),
+      onClosing: () {
+        Navigator.of(context).pop();
+      },
+    );
+  }
+}
+
+class RSearchModalItem<T> {
+  RSearchModalItem({
+    required this.value,
+    required this.label,
+  });
+
+  final T value;
+  final String label;
+}
---
+++ test/components/organisms/search_modal/search_modal_test.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rarUI/rarui.dart';
+
+void main() {
+  late Key buttonKey;
+  late Key rSearchModalKey;
+  late Widget widget;
+  late List<String> options;
+  String selectedValue = "";
+
+  setUpAll(() {
+    buttonKey = Key("button-key");
+    rSearchModalKey = Key("search-modal-key");
+    options = [
+      "001 BCO DO BRASIL S.A",
+      "003 BCO DA AMAZONIA S.A",
+      "004 BCO DO NORDESTE DO BRASIL S.A",
+      "010 CREDICOAMO",
+      "012 BANCO INBURSA",
+    ];
+
+    widget = MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              StatefulBuilder(
+                builder: (BuildContext context, setState) => RElevatedButton(
+                  key: buttonKey,
+                  onPressed: () => showModalBottomSheet(
+                    context: context,
+                    builder: (context) => RSearchModal<String>(
+                      key: rSearchModalKey,
+                      title: "Nome do banco",
+                      subtitle:
+                          "Digite o nome do banco ou o número da agência.",
+                      searchLabel: "Busca",
+                      searchHintText: "Digite o nome da instituição",
+                      searchItems: options
+                          .map((option) =>
+                              RSearchModalItem(value: option, label: option))
+                          .toList(),
+                      onSelected: (value) {
+                        selectedValue = value;
+                      },
+                    ),
+                  ),
+                  text: "Search",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  });
+
+  testWidgets("RSearchModal: deve renderizar o componente corretamente",
+      (tester) async {
+    await tester.pumpWidget(widget);
+    final searchButtonFinder = find.byKey(buttonKey);
+    await tester.tap(searchButtonFinder);
+    await tester.pumpAndSettle();
+    final rSearchModalFinder = find.byKey(rSearchModalKey);
+    expect(rSearchModalFinder, findsOneWidget);
+    expect(find.text("Nome do banco"), findsOne);
+    expect(
+        find.text("Digite o nome do banco ou o número da agência."), findsOne);
+  });
+
+  testWidgets("RSearchModal: deve selecionar um itemcorretamente",
+      (tester) async {
+    await tester.pumpWidget(widget);
+    final searchButtonFinder = find.byKey(buttonKey);
+    await tester.tap(searchButtonFinder);
+    await tester.pumpAndSettle();
+    final rSearchModalFinder = find.byKey(rSearchModalKey);
+    expect(rSearchModalFinder, findsOneWidget);
+    final inputFinder = find.byType(RTextFormField);
+    await tester.enterText(inputFinder, "001");
+    await tester.pumpAndSettle();
+    final itemFinder = find.text("001 BCO DO BRASIL S.A");
+    expect(itemFinder, findsOne);
+    await tester.tap(itemFinder);
+    await tester.pumpAndSettle();
+    expect(selectedValue, "001 BCO DO BRASIL S.A");
+  });
+}
