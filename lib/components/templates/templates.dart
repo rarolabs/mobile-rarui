c816104bfef2e068c1dca30bbcec586b51971f40
Guilherme Campos <guilherme.campos@rarolabs.com.br>
2025-09-25 15:23:26 -0300

    ajustes para subir ao pub dev

--- lib/components/templates/templates.dart
+++ lib/components/templates/templates.dart
@@ -1,10 +1,5 @@
 // usada para exportar os templates
 library;
 
-export 'status/status.dart';
 export 'demo/material_demo_page.dart';
 export 'demo/theme_demo_page.dart';
-export 'password_code/password_code.dart';
-export 'qr_code/qr_code_reader_template.dart';
-export 'transfer_value/transfer_value.dart';
-export 'review_template/review_template.dart';
