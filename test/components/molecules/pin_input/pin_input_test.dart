e44570a59239e38e11973cf8d151d28fdc39640f
ext.amontenegro <ext.amontenegro@pine.com.br>
2026-03-06 16:29:31 -0300

    test: small changes to the backspace method tests

--- test/components/molecules/pin_input/pin_input_test.dart
+++ test/components/molecules/pin_input/pin_input_test.dart
@@ -18,8 +18,7 @@ void main() {
     );
   }
 
-  testWidgets('RPinInput deve renderizar corretamente',
-      (WidgetTester tester) async {
+  testWidgets('RPinInput deve renderizar corretamente', (WidgetTester tester) async {
     await tester.pumpWidget(buildTestWidget(length: 5));
 
     expect(find.byType(RPinInput), findsOneWidget);
@@ -28,9 +27,7 @@ void main() {
     expect(find.byType(RTextFormField), findsNWidgets(5));
   });
 
-  testWidgets(
-      'onComplete é chamado somente quando todos os campos estão preenchidos',
-      (WidgetTester tester) async {
+  testWidgets('onComplete é chamado somente quando todos os campos estão preenchidos', (WidgetTester tester) async {
     String? pinCode;
     final formKey = GlobalKey<FormState>();
 
@@ -61,8 +58,7 @@ void main() {
     expect(pinCode, equals('1234'));
   });
 
-  testWidgets('addDigit adiciona corretamente e chama onComplete ao final',
-      (WidgetTester tester) async {
+  testWidgets('addDigit adiciona corretamente e chama onComplete ao final', (WidgetTester tester) async {
     String? pinCode;
     final formKey = GlobalKey<FormState>();
 
@@ -81,8 +77,7 @@ void main() {
       ),
     );
 
-    final state =
-        tester.state<RPinInputState>(find.byKey(const Key('pin_input_widget')));
+    final state = tester.state<RPinInputState>(find.byKey(const Key('pin_input_widget')));
     state.addDigit('5');
     state.addDigit('4');
     state.addDigit('3');
@@ -94,11 +89,10 @@ void main() {
     expect(pinCode, equals('543'));
   });
 
-  testWidgets('backspace apaga corretamente o último dígito',
+  testWidgets('backspace apaga corretamente o digito do campo em foco - No exemplo foca no terceiro campo',
       (WidgetTester tester) async {
     await tester.pumpWidget(buildTestWidget(length: 3));
-    final state =
-        tester.state<RPinInputState>(find.byKey(const Key('pin_input_widget')));
+    final state = tester.state<RPinInputState>(find.byKey(const Key('pin_input_widget')));
 
     state.addDigit('1');
     state.addDigit('2');
@@ -107,9 +101,15 @@ void main() {
 
     expect(find.text('3'), findsOneWidget);
 
+    final fieldInFocus = find.byType(RTextFormField).at(2);
+
+    await tester.tap(fieldInFocus);
+
     state.backspace();
+
     await tester.pump();
     expect(find.text('3'), findsNothing);
     expect(find.text('2'), findsOneWidget);
+    expect(find.text('1'), findsOneWidget);
   });
 }
