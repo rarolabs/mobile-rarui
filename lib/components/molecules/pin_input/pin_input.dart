6698ad373de24db1c7aa6b1fb9876e1191effdc2
ext.amontenegro <ext.amontenegro@pine.com.br>
2026-03-07 09:38:43 -0300

    fix: new logic for deleting the last field containing a digit if the fields are out of focus

--- lib/components/molecules/pin_input/pin_input.dart
+++ lib/components/molecules/pin_input/pin_input.dart
@@ -88,17 +88,14 @@ class RPinInputState extends State<RPinInput> {
         spacing: widget.spacing,
         children: List.generate(widget.length, (index) {
           final isFocused = focusNodes[index].hasFocus;
-          final borderColor = allFilled
-              ? activeColor
-              : (isFocused ? activeColor : defaultColor);
+          final borderColor = allFilled ? activeColor : (isFocused ? activeColor : defaultColor);
 
           return SizedBox(
             width: widget.widthFormField,
             height: widget.heightFormField,
             child: Container(
               decoration: BoxDecoration(
-                border:
-                    Border.all(color: borderColor, width: widget.borderWidth),
+                border: Border.all(color: borderColor, width: widget.borderWidth),
                 borderRadius: BorderRadius.circular(8),
                 color: widget.backgroundColor ?? Colors.transparent,
               ),
@@ -193,11 +190,15 @@ class RPinInputState extends State<RPinInput> {
   }
 
   void backspace() {
-    final int index = focusNodes.indexWhere((node) => node.hasFocus);
+    int index = focusNodes.indexWhere((node) => node.hasFocus);
 
-    if (index == -1) return;
+    if (index == -1) {
+      index = controllers.lastIndexWhere((controller) => controller.text.isNotEmpty);
 
-    if (controllers[index].text.isNotEmpty) {
+      if (index == -1) return;
+      focusNodes[index].requestFocus();
+      controllers[index].clear();
+    } else if (controllers[index].text.isNotEmpty) {
       controllers[index].clear();
     } else if (index > 0) {
       focusNodes[index - 1].requestFocus();
